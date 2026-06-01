4dee1ed9b203b2013641e2d498e366e7d707cbbf
michele_monticchio <michelemonticchio@gmail.com>
2026-06-01 22:21:21 +0200

    Create Solution.java

---
+++ java/src/main/java/neetcode/arraysandhashing/p0217_containsduplicate/Solution.java
@@ -0,0 +1,25 @@
+package neetcode.arraysandhashing.p0217_containsduplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    /**
+     * Returns true if any value appears at least twice in the array,
+     * false if every element is distinct.
+     *
+     * Time:  O(n)
+     * Space: O(n)
+     */
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int n : nums) {
+            if (seen.contains(n)) {
+                return true;
+            }
+            seen.add(n);
+        }
+        return false;
+    }
+}
